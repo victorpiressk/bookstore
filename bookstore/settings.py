aa44c0732c6562d8c9d911c294ea7c550327df42
Victor Pires <victorpiressk@gmail.com>
2026-05-31 11:49:27 -0300

    feat: atualizado a configuração do banco de dados para conexão com Supabase

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
+        "OPTIONS": {
+            "options": f"-c search_path={os.environ.get('SQL_SCHEMA', 'public')}"
+        },
     }
 }
 
